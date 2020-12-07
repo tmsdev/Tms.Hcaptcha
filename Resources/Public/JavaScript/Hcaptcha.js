2b22c55af271c9c6d8c9d2cc24a03801fc85547d
Joachim Beck <beck@tms-institut.de>
2020-12-07 12:41:19 +0100

    TASK: lazyload hcaptcha widget

--- Resources/Public/JavaScript/Hcaptcha.js
+++ Resources/Public/JavaScript/Hcaptcha.js
@@ -3,3 +3,24 @@ function setHcaptchaHiddenFieldValue(response) {
     let activeFormCaptchaHiddenFieldId = activeFormCaptchaIFrame.parentElement.nextElementSibling.id;
     document.getElementById(activeFormCaptchaHiddenFieldId).value = response;
 }
+
+function renderHcpatchaWidget() {
+    let forms = document.querySelectorAll('form');
+    Array.prototype.forEach.call(forms, function (form) {
+        form.addEventListener('input', (e) => {
+            let widget = form.querySelector('.h-captcha');
+            if (widget && !widget.firstChild) {
+                hcaptcha.render(widget.getAttribute('id'));
+                let btn = form.querySelector('button[data-captcha-forceload]');
+                if (btn) {
+                    btn.style.display = 'none';
+                }
+            }
+        });
+    });
+}
+
+function forceRenderHcpatchaWidget(btn) {
+    hcaptcha.render(btn.getAttribute('data-captcha-forceload'));
+    btn.style.display = 'none';
+}
