defac80b18f8c713518d6e373611512ae2d5cb49
Joachim Beck <beck@tms-institut.de>
2021-02-10 12:05:07 +0100

    BUGFIX: make it work with Neos 5.3

--- Classes/Validation/Validator/IsHumanValidator.php
+++ Classes/Validation/Validator/IsHumanValidator.php
@@ -1,10 +1,12 @@
 <?php
 namespace Tms\Hcaptcha\Validation\Validator;
 
+use http\Encoding\Stream;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Http\Client\Browser;
 use Neos\Flow\Http\Client\CurlEngine;
-use Neos\Flow\Http\Uri;
+use Neos\Flow\Http\ContentStream;
+use Neos\Flow\Log\Utility\LogEnvironment;
 use Neos\Flow\Validation\Validator\AbstractValidator;
 use Psr\Log\LoggerInterface;
 
@@ -17,7 +19,7 @@ class IsHumanValidator extends AbstractValidator
      * @Flow\Inject
      * @var LoggerInterface
      */
-    protected $systemLogger;
+    protected $logger;
 
     /**
      * @var boolean
@@ -55,15 +57,16 @@ class IsHumanValidator extends AbstractValidator
         $arguments['response'] = $value;
 
         try {
-            $response = $browser->request($uri, 'POST', $arguments);
+            // TODO: needs testing - the third request() param "$arguments" does not work anymore since Neos 5.3
+            $response = $browser->request($uri, 'POST', [], [], [], http_build_query($arguments));
             $responseContentsArray = json_decode($response->getBody()->getContents(), true);
 
             if (!$responseContentsArray['success']) {
-                $this->systemLogger->error($response);
+                $this->logger->error(json_encode($responseContentsArray), LogEnvironment::fromMethodName(__METHOD__));
                 $this->addError('Captcha failed. Please try again.', 1600419210);
             }
         } catch (\Exception $e) {
-            $this->systemLogger->error($e->getMessage());
+            $this->logger->error($e->getMessage(), LogEnvironment::fromMethodName(__METHOD__));
             $this->addError('Captcha verification request failed. Please try again.', 1601958019);
         }
     }
